43f01483bf6a07b21d7db11f12ed295b99d7bb81
CavaloAmigavel <2230333@my.ipleiria.pt>
2023-10-31 19:14:13 +0000

    Commit inicial

---
+++ LootAR/LootAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LootAR
+//
+//  Created by Carol on 31/10/23.
+//  Copyright © 2023 Carol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
